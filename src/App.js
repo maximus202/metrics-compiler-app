a02ef2c05c29e960c57490c75e3dd363c72220dc
Max R <maxx.ratto@gmail.com>
2020-06-29 15:18:16 -0600

    Created basic structure for Upload component

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Upload from './components/Upload/Upload';
+
+function App() {
+  return (
+    <Upload />
+  );
+}
+
+export default App;
---
+++ src/components/Upload/Upload.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class Upload extends Component {
+    render() {
+        return (
+            <>
+            <header>
+                <h1>
+                    MetricsCompiler
+                </h1>
+            </header>
+            <main>
+                <h2>
+                    Upload an excel file below to generate a metrics report
+                </h2>
+                <form>
+                    <input type="file" ID="fileSelect" accept=".xlsx, .xls, .csv"/>
+                    <input type="submit" />
+                </form>
+                
+                
+            </main>
+            </>
+        )
+    }
+}
+
+export default Upload;
